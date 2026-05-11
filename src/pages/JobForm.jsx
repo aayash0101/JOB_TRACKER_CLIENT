2e65efe325ce5e9a10eb3fb779dd8862c3a88e9f
Aayash <dangaayash@gmail.com>
2026-05-11 13:41:27 +0545

    Call me back

--- src/pages/JobForm.jsx
+++ src/pages/JobForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { useNavigate, useParams, Link } from 'react-router-dom'
 import api from '../api/axios'
 
